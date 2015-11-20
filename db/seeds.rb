543a2090b437b2b7eb7e7d175721d2cbd1d36061
Jane <jane.e.reynolds@gmail.com>
2015-11-19 21:50:41 -0800

    Seed Mortgage_Payment calculator input field data, for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,19 @@
 end
 
 # Create Input Fields
-%w[].each do
+[{name: "Monthly income", tag: "text"}, {name: "Monthly debt", tag: "text"}, 
+	{name: "Interest rate", tag: "select"}, {name: "Term", tag: "select"}, {name:
+	"Down payment(%)", tag: "select"}, {name: "Loan amount", tag: "text"}].each do |input|
+	InputField.create(name: input[:name], tag: input[:tag])
+end
+
+
+# Mortgage Payment Input Fields
+loan = InputField.find_or_create_by(name: "Loan amount")
+rate = InputField.find_or_create_by(name: "Interest rate")
+term = InputField.find_or_create_by(name: "Term")
+
+mortgage_calc = Calculator.find_or_create_by(name: "Mortgage_Payment")
+mortgage_calc.input_fields<<loan
+mortgage_calc.input_fields<<rate
+mortgage_calc.input_fields<<term
