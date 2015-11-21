2a16a8249f2866139037122b9b8d9d9d2ea084b9
Jane <jane.e.reynolds@gmail.com>
2015-11-20 19:48:53 -0800

    Display form fields for example calculator

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -3,5 +3,16 @@ class CalculatorsController < ApplicationController
   end
 
   def show
+  	@calculator = Calculator.find(params[:id])
   end
+  
+  def calculate
+  	@result = 42
+  end
+  
+  private
+  
+  	def calculator_params
+  		params.require(:calculator).permit(:name, :input_fields)
+  	end
 end
