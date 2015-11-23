c18c17d0e892f15235346a96140fe1f1e1da87b8
Jane <jane.e.reynolds@gmail.com>
2015-11-23 12:52:27 -0800

    Update calculator#show view to display multiple results (from @results hash)

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -2,6 +2,7 @@ class Calculator < ActiveRecord::Base
 	has_and_belongs_to_many :input_fields
 	has_and_belongs_to_many :output_fields
 
+	# Must return a hash with three keys: Monthly payment, Total payments, Total interest
 	def self.mortgage
 		result = {"Monthly payment" => 42, "Total payments" => 37, "Total interest" => 3}
 	end
